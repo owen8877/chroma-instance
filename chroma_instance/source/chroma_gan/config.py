ca66ac08298d20191445d72089ab8c67af0dcdea
owen8877 <owentoget@yeah.net>
2020-11-23 11:20:58 -0600

    chromaGAN able to train

--- chroma_instance/source/chroma_gan/config.py
+++ chroma_instance/source/chroma_gan/config.py
@@ -1,7 +1,7 @@
 import os
+import machine
 
 # DIRECTORY INFORMATION
-# DATASET = "imagenet" # UPDATE
 TEST_NAME = "FirstTest"
 ROOT_DIR = os.path.abspath('../../')
 DATA_DIR = os.path.join(ROOT_DIR, 'dataset/')
@@ -12,13 +12,18 @@ LOG_DIR = os.path.join(ROOT_DIR, 'logs/chroma_gan/')
 for dir in (DATA_DIR, OUT_DIR, MODEL_DIR, LOG_DIR):
     os.makedirs(dir, exist_ok=True)
 
-TRAIN_DIR = "train"  # UPDATE
-TEST_DIR = "test"  # UPDATE
+TRAIN_DIR = "train"
+TEST_DIR = "test"
 
 # DATA INFORMATION
 IMAGE_SIZE = 224
-BATCH_SIZE = 1
 
 # TRAINING INFORMATION
-PRETRAINED = "imagenet.h5"  # UPDATE
-NUM_EPOCHS = 5
+PRETRAINED = "imagenet.h5"
+if machine.is_colab:
+    NUM_EPOCHS = 5
+    BATCH_SIZE = 10
+else:
+    NUM_EPOCHS = 2
+    BATCH_SIZE = 6
+
